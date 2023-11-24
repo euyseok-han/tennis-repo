f99106b83092414d31812fc9b0b6a5947c97a676
euyseok-han <euyseok.han@wesang.com>
2023-11-25 03:17:17 +0900

    for deploy

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -4,10 +4,10 @@ set -e
 
 RUN_MANAGE_PY='poetry run python -m core.manage'
 
-echo 'Collecting static files...'
-$RUN_MANAGE_PY collectstatic --no-input
+#echo 'Collecting static files...'
+#$RUN_MANAGE_PY collectstatic --no-input
 
 echo 'Running migrations...'
 $RUN_MANAGE_PY migrate --no-input
 
-exec poetry run daphne tennis_match.tennis_match.asgi:application -p 8000 -b 0.0.0.0
+exec poetry run daphne core.project.asgi:application -p 8000 -b 0.0.0.0
